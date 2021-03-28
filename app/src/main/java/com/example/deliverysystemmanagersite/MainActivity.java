675cf4914e782bba5874e8ce9d456e967ffd9ff4
Lirunze <617172142@qq.com>
2021-03-28 17:01:55 +0100

    使用ROOM作为sqlite框架

--- app/src/main/java/com/example/deliverysystemmanagersite/MainActivity.java
+++ app/src/main/java/com/example/deliverysystemmanagersite/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.deliverysystemmanagersite;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.deliverysystemmanagersite.Test.AppDatabase;
+import com.example.deliverysystemmanagersite.Test.User;
+import com.example.deliverysystemmanagersite.Test.UserDao;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity{
     TextView textView;
+    AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +26,27 @@ public class MainActivity extends AppCompatActivity {
     public void init(){
         textView = (TextView)findViewById(R.id.tv);
         textView.setText("test!!");
+
+        //增删改查都需要在子线程操作
+        new Thread(() -> {
+            db = Room.databaseBuilder(getApplicationContext(),
+                    AppDatabase.class, "database-name").build();
+
+            //增加数据测试
+            for (int i = 0; i < 10; i++) {
+                User user = new User();
+                user.setUid(i);
+                user.setFirstName("Shell" + i);
+                db.userDao().insertAll(user);
+            }
+
+            for (User user : db.userDao().getAll()) {
+                System.out.println(user);
+            }
+
+        }).start();
+
     }
+
+
 }
